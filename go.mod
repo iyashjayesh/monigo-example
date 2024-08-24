069ce4f2769c8244577a2e8bc1838e50821aa5a5
Yash Chauhan <er.yashjayeshchauhan@gmail.com>
2024-08-25 01:54:22 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module monigo-example
+
+replace github.com/iyashjayesh/monigo => ../monigo
+
+go 1.21.0
+
+require (
+	github.com/iyashjayesh/monigo v0.0.0-20240824202226-047e547f41fb
+	golang.org/x/exp v0.0.0-20240823005443-9b4947da3948
+)
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/iyashjayesh/monigo"
+	"golang.org/x/exp/rand"
+)
+
+func main() {
+
+	go monigo.ServeDashboard(":8080", "MoniGo-example-microService")
+
+	http.HandleFunc("/api", apiHandler)
+	http.ListenAndServe(":8000", nil)
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
+	monigo.MeasureExecutionTime("MemExpensiveFunc", memexpensiveFunc)
+	monigo.MeasureExecutionTime("CpuExpensiveFunc", cpuexpensiveFunc)
+	monigo.RecordRequestDuration(time.Since(start))
+	w.Write([]byte("API response"))
+}
+
+//go:noinline
+func memexpensiveFunc() {
+	m := make([]int, 10_000_000)
+	for i := range m {
+		m[i] = rand.Intn(127)
+	}
+
+	memanotherExpensiveFunc()
+}
+
+//go:noinline
+func memanotherExpensiveFunc() {
+	m := make(map[int]float64, 1_000_000)
+
+	for key := range m {
+		m[key] = rand.Float64()
+	}
+}
+
+//go:noinline
+func cpuexpensiveFunc() {
+	var sum float64
+	for i := 0; i < 10_000_000; i++ {
+		sum += rand.Float64()
+	}
+
+	anotherExpensiveFunc()
+}
+
+//go:noinline
+func anotherExpensiveFunc() {
+	var sum int
+	for i := 0; i < 1_000_000; i++ {
+		sum += rand.Intn(10)
+	}
+}
